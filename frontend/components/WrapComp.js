27342bdda9f4af9b9ce7042cf11226d8c4ef7647
Aman-Mandal <002amankumar@gmail.com>
2023-10-06 15:45:54 +0530

    UI fixes and price feed from oracle

--- frontend/components/WrapComp.js
+++ frontend/components/WrapComp.js
@@ -1,15 +1,15 @@
-import { useWeb3Modal } from "@web3modal/react";
-import Image from "next/image";
-import React, { useEffect, useState } from "react";
-import { HiMiniArrowsUpDown } from "react-icons/hi2";
-import { useAccount } from "wagmi";
-import { waitForTransaction, writeContract, readContract } from "@wagmi/core";
-import { ABI, superToken } from "@/constants";
-import { parseEther } from "viem";
-import { ToastContainer, toast } from "react-toastify";
-import "react-toastify/dist/ReactToastify.css";
-import { useBalance } from "wagmi";
-import Loader from "./Loader";
+import { useWeb3Modal } from '@web3modal/react';
+import Image from 'next/image';
+import React, { useEffect, useState } from 'react';
+import { HiMiniArrowsUpDown } from 'react-icons/hi2';
+import { useAccount } from 'wagmi';
+import { waitForTransaction, writeContract, readContract } from '@wagmi/core';
+import { ABI, superToken } from '@/constants';
+import { parseEther } from 'viem';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import { useBalance } from 'wagmi';
+import Loader from './Loader';
 
 const WrapComp = () => {
   const [wrap, setWrap] = useState(true);
@@ -34,11 +34,11 @@ const WrapComp = () => {
       const data = await readContract({
         address: superToken,
         abi: ABI,
-        functionName: "show",
+        functionName: 'show',
       });
       setPrize(data);
     } catch (e) {
-      console.log("getPrice", e);
+      console.log('getPrice', e);
     }
   };
 
@@ -52,16 +52,16 @@ const WrapComp = () => {
       const { hash } = await writeContract({
         address: superToken,
         abi: ABI,
-        functionName: "wrap",
+        functionName: 'wrap',
         args: [parseEther(value)],
         value: parseEther(value),
       });
       await waitForTransaction({ hash });
       setLoading(false);
-      toast.success("Wrapped XDC to sXDC");
+      toast.success('Wrapped XDC to sXDC');
     } catch (e) {
       setLoading(false);
-      console.log("wrap", e);
+      console.log('wrap', e);
     }
   };
   const unWrapFn = async () => {
@@ -70,15 +70,15 @@ const WrapComp = () => {
       const { hash } = await writeContract({
         address: superToken,
         abi: ABI,
-        functionName: "unwrap",
+        functionName: 'unwrap',
         args: [parseEther(value)],
       });
       await waitForTransaction({ hash });
       setLoading(false);
-      toast.success("Unwrapped sXDC to XDC");
+      toast.success('Unwrapped sXDC to XDC');
     } catch (e) {
       setLoading(false);
-      console.log("wrap", e);
+      console.log('wrap', e);
     }
   };
 
@@ -89,8 +89,8 @@ const WrapComp = () => {
     return null;
   }
   return (
-    <div className="bg-[#1C1D20] rounded-xl w-[600px] py-10 px-6 text-white font-Poppins">
-      <div className="flex mb-5 gap-2">
+    <div className='bg-[#1C1D20] rounded-xl w-[600px] py-10 px-6 text-white font-Poppins'>
+      <div className='flex mb-5 gap-2'>
         <p
           onClick={() => {
             setWrap(true);
@@ -98,10 +98,9 @@ const WrapComp = () => {
           }}
           className={`${
             wrap
-              ? "bg-green-700/20 text-green-500 hover:bg-green-700/40"
-              : "text-gray-400"
-          }  hover:bg-[#2A2B2E] py-2 px-6 rounded-md cursor-pointer`}
-        >
+              ? 'bg-green-700/20 text-green-500 hover:bg-green-700/40'
+              : 'text-gray-400'
+          }  hover:bg-[#2A2B2E] py-2 px-6 rounded-md cursor-pointer`}>
           Wrap
         </p>
         <p
@@ -111,113 +110,112 @@ const WrapComp = () => {
           }}
           className={`${
             unwrap
-              ? "bg-green-700/20 text-green-500 hover:bg-green-700/40"
-              : "text-gray-400"
-          }  hover:bg-[#2A2B2E] py-2 px-6 rounded-md cursor-pointer`}
-        >
+              ? 'bg-green-700/20 text-green-500 hover:bg-green-700/40'
+              : 'text-gray-400'
+          }  hover:bg-[#2A2B2E] py-2 px-6 rounded-md cursor-pointer`}>
           Unwrap
         </p>
       </div>
 
       <div className={`flex flex-col  gap-3 relative`}>
-        <div className="flex justify-between bg-[#2A2B2E] rounded-2xl px-4 py-4 items-center relative">
+        <div className='flex justify-between bg-[#2A2B2E] rounded-2xl px-4 py-4 items-center relative'>
           <div>
             <input
-              type="number"
-              placeholder="0.0"
+              type='number'
+              placeholder='0.0'
               className={`py-3 outline-none bg-inherit text-3xl `}
               onWheel={(event) => event.target.blur()}
-              id="wrapAmount"
+              id='wrapAmount'
               onChange={(e) => {
                 setValue(e.target.value);
               }}
               value={value}
             />
-            <p className="text-sm text-gray-400 ">
+            <p className='text-sm text-gray-400 '>
               ${(value * Number(prize)) / 10000}
             </p>
           </div>
 
           {unwrap ? (
             <div>
-              <div className="flex gap-2 items-center bg-[#1C1D20] py-3 p-3 rounded-md mb-2 w-[120px]">
+              <div className='flex gap-2 items-center bg-[#1C1D20] py-3 p-3 rounded-md mb-2 w-[120px]'>
                 <Image
-                  src="/xdc.png"
+                  src='/xdc.png'
                   height={30}
                   width={30}
-                  alt="xdc"
-                  className="rounded-full border-2 border-yellow-400"
+                  alt='xdc'
+                  className='rounded-full border-2 border-yellow-400'
                 />
-                <p className="font-medium">sXDC</p>
+                <p className='font-medium'>sXDC</p>
               </div>
-              <p className="text-sm text-gray-400 text-right">
+              <p className='text-sm text-gray-400 text-right'>
                 Balance: {parseFloat(sxdcBalance?.formatted).toFixed(2)}
               </p>
             </div>
           ) : (
             <div>
-              <div className="flex gap-2 items-center bg-[#1C1D20] py-3 pl-3 rounded-md mb-2 w-[120px]">
+              <div className='flex gap-2 items-center bg-[#1C1D20] py-3 pl-3 rounded-md mb-2 w-[120px]'>
                 <Image
-                  src="/xdc.png"
+                  src='/xdc.png'
                   height={30}
                   width={30}
-                  alt="xdc"
-                  className="rounded-full"
+                  alt='xdc'
+                  className='rounded-full'
                 />
-                <p className="font-medium">XDC</p>
+                <p className='font-medium'>XDC</p>
               </div>
-              <p className="text-sm text-gray-400 text-right">
+              <p className='text-sm text-gray-400 text-right'>
                 Balance: {parseFloat(xdcBalance?.formatted).toFixed(2)}
               </p>
             </div>
           )}
         </div>
 
-        <div className="flex justify-between bg-[#2A2B2E] rounded-2xl px-4 py-4 items-center">
+        <div className='flex justify-between bg-[#2A2B2E] rounded-2xl px-4 py-4 items-center'>
           <div>
             <input
-              type="number"
+              type='number'
               disabled={wrap}
-              placeholder="0.0"
+              placeholder='0.0'
               className={`py-3 outline-none bg-inherit text-3xl cursor-not-allowed`}
               onWheel={(event) => event.target.blur()}
-              id="unWrapAmount"
+              id='unWrapAmount'
               value={value}
             />
-            <p className="text-sm text-gray-400 ">
+            <p className='text-sm text-gray-400 '>
               ${(value * Number(prize)) / 10000}
             </p>
           </div>
 
           {wrap ? (
             <div>
-              <div className="flex gap-2 items-center bg-[#1C1D20] py-3 p-3 rounded-md mb-2 w-[120px]">
+              <div className='flex gap-2 items-center bg-[#1C1D20] py-3 p-3 rounded-md mb-2 w-[120px]'>
                 <Image
-                  src="/xdc.png"
+                  src='/xdc.png'
                   height={30}
                   width={30}
-                  alt="xdc"
-                  className="rounded-full border-2 border-yellow-400"
+                  alt='xdc'
+                  className='rounded-full border-2 border-yellow-400'
                 />
-                <p className="font-medium">sXDC</p>
+                <p className='font-medium'>sXDC</p>
               </div>
-              <p className="text-sm text-gray-400 text-right">
+              <p className='text-sm text-gray-400 text-right'>
                 Balance: {parseFloat(sxdcBalance?.formatted).toFixed(2)}
               </p>
             </div>
           ) : (
             <div>
-              <div className="flex gap-2 items-center bg-[#1C1D20] py-3 pl-3 rounded-md mb-2 w-[120px]">
+              <div className='flex gap-2 items-center bg-[#1C1D20] py-3 pl-3 rounded-md mb-2 w-[120px]'>
                 <Image
-                  src="/xdc.png"
+                  src='/xdc.png'
                   height={30}
                   width={30}
-                  alt="xdc"
-                  className="rounded-full"
+                  alt='xdc'
+                  className='rounded-full'
                 />
-                <p className="font-medium">XDC</p>
+                <p className='font-medium'>XDC</p>
               </div>
-              <p className="text-sm text-gray-400 text-right">
+              <p className='text-sm text-gray-400 text-right'>
                 Balance: {parseFloat(xdcBalance?.formatted).toFixed(2)}
               </p>
             </div>
@@ -229,32 +227,34 @@ const WrapComp = () => {
             setUnwrap(!unwrap);
             setWrap(!wrap);
           }}
-          className="bg-green-900 p-2 cursor-pointer rounded-full w-fit text-green-300 absolute left-[46%] top-[42%]"
-        >
+          className='bg-green-900 p-2 cursor-pointer rounded-full w-fit text-green-300 absolute left-[46%] top-[42%]'>
           <HiMiniArrowsUpDown size={20} />
         </div>
       </div>
 
-      <p className="text-center mt-4 text-gray-300">1 sXDC = 1 XDC</p>
+      <p className='text-center mt-4 text-gray-300'>
+        1 sXDC = 1 XDC{' '}
+        <span className='text-sm text-gray-400 ml-1'>
+          (${Number(Number(prize) / 10000).toFixed(4)})
+        </span>
+      </p>
 
       {isConnected ? (
         <button
-          type="submit"
-          className="bg-green-700/20 w-full mt-8 text-green-500 py-3 hover:bg-green-700/40  tracking-wider   rounded-lg "
-          onClick={wrap ? wrapFn : unWrapFn}
-        >
+          type='submit'
+          className='bg-green-700/20 w-full mt-8 text-green-500 py-3 hover:bg-green-700/40  tracking-wider   rounded-lg '
+          onClick={wrap ? wrapFn : unWrapFn}>
           {loading ? (
             <Loader inComp={true} />
           ) : (
-            <p>{wrap ? "Wrap" : "Unwrap"} Token</p>
+            <p>{wrap ? 'Wrap' : 'Unwrap'} Token</p>
           )}
         </button>
       ) : (
         <button
-          type="button"
+          type='button'
           onClick={open}
-          className="bg-green-700/20 w-full mt-8 text-green-500 py-3 hover:bg-green-700/40  tracking-wider   rounded-lg "
-        >
+          className='bg-green-700/20 w-full mt-8 text-green-500 py-3 hover:bg-green-700/40  tracking-wider   rounded-lg '>
           Connect Wallet
         </button>
       )}
--- frontend/pages/dashboard/[did].js
+++ frontend/pages/dashboard/[did].js
@@ -1,14 +1,14 @@
-import Image from "next/image";
-import React, { useState, useEffect } from "react";
-import { useAccount } from "wagmi";
-import { readContract, writeContract, waitForTransaction } from "@wagmi/core";
-import { ABI, superToken } from "@/constants";
-import { useRouter } from "next/router";
-import { etherUnits, formatEther, formatUnits } from "viem";
-import { ToastContainer, toast } from "react-toastify";
-import "react-toastify/dist/ReactToastify.css";
+import Image from 'next/image';
+import React, { useState, useEffect } from 'react';
+import { useAccount } from 'wagmi';
+import { readContract, writeContract, waitForTransaction } from '@wagmi/core';
+import { ABI, superToken } from '@/constants';
+import { useRouter } from 'next/router';
+import { etherUnits, formatEther, formatUnits } from 'viem';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
-import Loader from "@/components/Loader";
+import Loader from '@/components/Loader';
 const CurrentStream = ({}) => {
   const router = useRouter();
   const id = router.query.did;
@@ -17,30 +17,30 @@ const CurrentStream = ({}) => {
   const [balance, setBalance] = useState(0);
   const [data, setData] = useState();
   const [count, setCount] = useState(0);
+  const [isCounting, setIsCounting] = useState(true);
 
   const getData = async () => {
     try {
       const dataStr = await readContract({
         address: superToken,
         abi: ABI,
-        functionName: "getOutgoingStreams",
+        functionName: 'getOutgoingStreams',
         args: [address],
       });
 
-      console.log("dd", dataStr[id]);
       setData(dataStr[id]);
       if (!dataStr[id].isOnGoing) {
         const balance = await readContract({
           address: superToken,
           abi: ABI,
-          functionName: "tokensSentTillDate",
+          functionName: 'tokensSentTillDate',
           args: [address, dataStr[id].participant],
         });
 
         setBalance(parseFloat(formatEther(BigInt(balance))).toFixed(6));
       }
     } catch (e) {
-      console.log("dashboard data", e);
+      console.log('dashboard data', e);
     }
   };
 
@@ -54,9 +54,8 @@ const CurrentStream = ({}) => {
       const timestampInMilliseconds = BigInt(timestamp) * 1000n;
       const date = new Date(Number(timestampInMilliseconds));
 
-      // You can format the date as per your preference using Date methods
-      const formattedDate = date.toDateString(); // Converts to a string like "Tue Oct 05 2021"
-      const formattedTime = date.toLocaleTimeString(); // Converts to a string like "11:31:05 AM"
+      const formattedDate = date.toDateString();
+      const formattedTime = date.toLocaleTimeString();
       return [formattedDate, formattedTime];
     }
     return [];
@@ -66,16 +65,18 @@ const CurrentStream = ({}) => {
     // logic here
     try {
       setLoading(true);
+
       const { hash } = await writeContract({
         address: superToken,
         abi: ABI,
-        functionName: "stopStream",
+        functionName: 'stopStream',
         args: [data?.participant],
       });
 
       await waitForTransaction({ hash });
-      toast.success("Stream Cancelled");
+      toast.success('Stream Cancelled');
       setLoading(false);
+      setIsCounting(false);
     } catch (error) {
       console.log(error);
       setLoading(false);
@@ -97,46 +98,47 @@ const CurrentStream = ({}) => {
       ).toFixed(6);
 
       setCount(newCount);
-
-      animationFrameId = requestAnimationFrame(updateCount);
+      if (isCounting) {
+        animationFrameId = requestAnimationFrame(updateCount);
+      }
     };
 
     updateCount();
 
     return () => cancelAnimationFrame(animationFrameId);
-  }, [data, Date.now() / 1000]);
+  }, [data, isCounting]);
 
   return (
-    <div className="bg-[#141414] h-[100vh] flex flex-col text-white font-Poppins px-20 py-20  items-center">
-      <div className="mt-28 w-[700px]">
-        <p className="text-center text-gray-400 mb-3">Total Amount Streamed</p>
-        <div className="flex gap-4 items-center justify-center">
+    <div className='bg-[#141414] h-[100vh] flex flex-col text-white font-Poppins px-20 py-20  items-center'>
+      <div className='mt-28 w-[700px]'>
+        <p className='text-center text-gray-400 mb-3'>Total Amount Streamed</p>
+        <div className='flex gap-4 items-center justify-center'>
           <Image
-            src="/xdc.png"
+            src='/xdc.png'
             height={50}
             width={50}
-            alt="usdt"
-            className="rounded-full border-2 border-yellow-400"
+            alt='usdt'
+            className='rounded-full border-2 border-yellow-400'
           />
-          <p className="text-6xl">
+          <p className='text-6xl'>
             {data?.isOnGoing ? Number(count / 1000).toFixed(6) : balance}
           </p>
-          <p className="mt-4 text-green-400 text-lg">sXDC</p>
+          <p className='mt-4 text-green-400 text-lg'>sXDC</p>
         </div>
 
-        <div className="w-fit mt-14">
-          <div className="flex gap-[335px] ml-4 mb-2 text-gray-300">
+        <div className='w-fit mt-14'>
+          <div className='flex gap-[335px] ml-4 mb-2 text-gray-300'>
             <p>Sender</p>
             <p>Reciever</p>
           </div>
 
-          <div className="flex items-center">
-            <div className="flex gap-3 bg-[#1C1D20] py-4 px-6 rounded-lg w-[300px] items-center border-[0.5px] border-gray-600">
+          <div className='flex items-center'>
+            <div className='flex gap-3 bg-[#1C1D20] py-4 px-6 rounded-lg w-[300px] items-center border-[0.5px] border-gray-600'>
               <Image
-                src="/gif3.webp"
+                src='/gif3.webp'
                 height={40}
                 width={40}
-                className="rounded-md"
+                className='rounded-md'
               />
 
               <p>
@@ -144,14 +146,19 @@ const CurrentStream = ({}) => {
               </p>
             </div>
 
-            <Image src="/stream.gif" height={40} width={90} alt="stream" />
+            <Image
+              src='/stream.gif'
+              height={40}
+              width={90}
+              alt='stream'
+            />
 
-            <div className="flex gap-3 bg-[#1C1D20] py-4 px-6 rounded-lg w-[300px] items-center border-[0.5px] border-gray-600">
+            <div className='flex gap-3 bg-[#1C1D20] py-4 px-6 rounded-lg w-[300px] items-center border-[0.5px] border-gray-600'>
               <Image
-                src="/gif2.gif"
+                src='/gif2.gif'
                 height={40}
                 width={40}
-                className="rounded-md"
+                className='rounded-md'
               />
 
               <p>
@@ -161,31 +168,30 @@ const CurrentStream = ({}) => {
           </div>
         </div>
 
-        <div className="mt-28 flex w-full gap-10">
-          <div className="flex-[0.5] flex flex-col gap-2">
-            <p className="flex justify-between items-center text-gray-300">
+        <div className='mt-28 flex w-full gap-10'>
+          <div className='flex-[0.5] flex flex-col gap-2'>
+            <p className='flex justify-between items-center text-gray-300'>
               Start Date : <span>{getDate()[0]}</span>
             </p>
-            <p className="flex justify-between items-center text-gray-300">
+            <p className='flex justify-between items-center text-gray-300'>
               Time :<span>{getDate()[1]}</span>
             </p>
           </div>
-          <div className="flex-[0.5]  flex flex-col gap-2">
-            <p className="flex justify-between items-center text-gray-300">
+          <div className='flex-[0.5]  flex flex-col gap-2'>
+            <p className='flex justify-between items-center text-gray-300'>
               flow Rate : <span>{formatEther(data?.flowRate || 0)}</span>
             </p>
-            <p className="flex justify-between items-center text-gray-300">
+            <p className='flex justify-between items-center text-gray-300'>
               Network Name : <span>XDC Mainnet</span>
             </p>
-          </div>{" "}
+          </div>{' '}
         </div>
 
-        {data?.isOnGoing && (
+        {data?.isOnGoing && isCounting && (
           <button
             onClick={cancelStreamHandler}
-            className="bg-red-400/30 mt-10 w-full text-red-300 py-3 hover:bg-red-500/20  tracking-wider   rounded-lg "
-          >
-            {loading ? <Loader inComp={true} /> : "Canel Stream"}
+            className='bg-red-400/30 mt-10 w-full text-red-300 py-3 hover:bg-red-500/20  tracking-wider   rounded-lg '>
+            {loading ? <Loader inComp={true} /> : 'Canel Stream'}
           </button>
         )}
       </div>
